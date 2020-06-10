dca2e2e4339961d1e2d1b6c8afcd628dc58a939c
Matteo Casucci <matteocasucci10@gmail.com>
2020-06-10 16:20:08 +0200

    Aggiungere i file di progetto.

---
+++ ClienttDemo/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClienttDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void GetSum_Click(object sender, RoutedEventArgs e)
+        {
+            double ris = 0;
+
+            int a = Convert.ToInt32(txtN1.Text);
+            int b = Convert.ToInt32(txtN2.Text);
+            int c = Convert.ToInt32(txtN3.Text);
+            int d = Convert.ToInt32(txtN4.Text);
+
+            HttpClient client= new HttpClient();
+
+            string uri = $"https://localhost:44383/api/Operations/GetSum?a={a}&b={b}&c={c}&d={d}";
+            HttpResponseMessage response = await client.GetAsync(uri);
+            string content = await response.Content.ReadAsStringAsync();
+            ris = JsonConvert.DeserializeObject<int>(content);
+            Dispatcher.Invoke(() => lblRis.Content = ris);
+        }
+
+        private async void GetProduct_Click(object sender, RoutedEventArgs e)
+        {
+            double ris = 0;
+
+            int a = Convert.ToInt32(txtNum1.Text);
+            int b = Convert.ToInt32(txtNum2.Text);
+            int c = Convert.ToInt32(txtNum3.Text);
+
+            HttpClient client = new HttpClient();
+
+            string uri = $"https://localhost:44383/api/Operations/GetProduct?a={a}&b={b}&c={c}";
+            HttpResponseMessage response = await client.GetAsync(uri);
+            string content = await response.Content.ReadAsStringAsync();
+            ris = JsonConvert.DeserializeObject<int>(content);
+            Dispatcher.Invoke(() => lblRis1.Content = ris);
+        }
+
+        private async void GetSubtraction_Click(object sender, RoutedEventArgs e)
+        {
+            double ris = 0;
+
+            int a = Convert.ToInt32(txtNumer1.Text);
+            int b = Convert.ToInt32(txtNumer2.Text);
+
+            HttpClient client = new HttpClient();
+
+            string uri = $"https://localhost:44383/api/Operations/GetSubtraction?a={a}&b={b}";
+            HttpResponseMessage response = await client.GetAsync(uri);
+            string content = await response.Content.ReadAsStringAsync();
+            ris = JsonConvert.DeserializeObject<int>(content);
+            Dispatcher.Invoke(() => lblRis2.Content = ris);
+
+        }
+    }
+}
---
+++ WebDemo/Controllers/OperationsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebDemo.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        // GET: api/<OperationsController>
+        [HttpGet]
+        public double GetSum(int a, int b,int c,int d)
+        {
+            return a + b + c + d;
+        }
+
+        [HttpGet]
+        public double GetProduct(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+
+        [HttpGet]
+
+        public double GetSubtraction(int a,int b)
+        {
+            return a - b;
+        }
+        // GET api/<OperationsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OperationsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<OperationsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OperationsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
